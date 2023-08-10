a5649a5a07ae5f287bc5a9704a40f7eec5745ef1
Juan Mercado <juan.mercado1986@gmail.com>
2023-08-10 10:40:21 -0300

    Se agregar el tests de la grabacion en memoria

---
+++ src/com/backend/test/OdontologoServiceTestEnMemoria.java
@@ -0,0 +1,26 @@
+package com.backend.test;
+
+import com.backend.dao.impl.OdontologoDaoEnMemoria;
+import com.backend.entity.Odontologo;
+import com.backend.service.OdontologoService;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class OdontologoServiceTestEnMemoria {
+
+    @Test
+    public void grabarEnMemoriaTest(){
+        OdontologoService odontologoServiceEnMemoria = new OdontologoService(new OdontologoDaoEnMemoria());
+
+        odontologoServiceEnMemoria.registrarOdontologo(new Odontologo(1, 10, "Juan", "Mercado"));
+        odontologoServiceEnMemoria.registrarOdontologo(new Odontologo(1, 10, "Evangelina", "Britos"));
+
+        List<Odontologo> listaDeOdontologos = odontologoServiceEnMemoria.listarOdontologos();
+
+        assertFalse(listaDeOdontologos.isEmpty());
+        assertEquals(2, listaDeOdontologos.size());
+    }
+}

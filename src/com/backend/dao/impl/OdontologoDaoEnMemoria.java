7669184e1be823fdc5999cbf7644b3e6227e8110
Juan Mercado <juan.mercado1986@gmail.com>
2023-08-10 11:09:28 -0300

    Se cambia la estructura en donde se guardan los odontologos en memoria

--- src/com/backend/dao/impl/OdontologoDaoEnMemoria.java
+++ src/com/backend/dao/impl/OdontologoDaoEnMemoria.java
@@ -1,32 +1,34 @@
 package com.backend.dao.impl;
-
 import com.backend.dao.Idao;
 import com.backend.entity.Odontologo;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import org.apache.log4j.Logger;
+import java.util.Map;
 
 public class OdontologoDaoEnMemoria implements Idao<Odontologo> {
 
     private static final Logger LOGGER = Logger.getLogger(OdontologoDaoEnMemoria.class);
 
-    private List<Odontologo> odontologosEnMemoria;
+    private Map<Integer, Odontologo> odontologosEnMemoria;
 
     public OdontologoDaoEnMemoria(){
-        odontologosEnMemoria = new ArrayList<>();
+        odontologosEnMemoria = new HashMap<Integer, Odontologo>();
     }
 
     @Override
     public Odontologo registrarOdonlotolos(Odontologo odontologo) {
-        odontologosEnMemoria.add(odontologo);
+        odontologosEnMemoria.put(odontologo.getId(), odontologo);
         LOGGER.info("El odontologo "+odontologo.getNombre()+" se a registrado");
         return odontologo;
     }
 
     @Override
     public List<Odontologo> listarOdontologos() {
-        LOGGER.info("Se obtuvieron "+odontologosEnMemoria.size()+" Odontologos");
-        return odontologosEnMemoria;
+        List<Odontologo> listaDeOdontologos = new ArrayList<>(odontologosEnMemoria.values());
+        LOGGER.info("Se obtuvieron "+listaDeOdontologos.size()+" Odontologos");
+        return listaDeOdontologos;
     }
 }
